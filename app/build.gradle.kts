e39489e9bfd131221432505a0f643d9fcfb688f3
acusi004 <dotrunghieu7490@gmail.com>
2023-11-17 09:52:32 +0700

    hieu update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.material:material:1.12.0-alpha01")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("androidx.cardview:cardview:1.0.0")
 
 
 }
